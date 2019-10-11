68e003eac6aa3c0418739478ec63cdd66a56e1e0
dban73 <53410595+dban73@users.noreply.github.com>
2019-10-11 01:29:28 -0400

    implemento API de google Mapas * se creo el activity map y las credenciales necesarias para su funcionamiento

--- app/src/main/java/com/example/quirquirutas/MainActivity.java
+++ app/src/main/java/com/example/quirquirutas/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.quirquirutas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.nio.channels.InterruptedByTimeoutException;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_ing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void siguiente(View view){
+        Intent i = new Intent(this,MapsActivity1.class);
+        startActivity(i);
     }
 }
